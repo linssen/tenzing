c79f2758e11540c28656be42814ca579c7c71fc6
Wil Linssen <wil@linssen.me>
2014-04-17 23:56:21 +0100

    Quick and very dirty example

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var clean, concat, gulp, sass, styleguide;
+
+clean = require('gulp-clean');
+concat = require('gulp-concat');
+gulp = require('gulp');
+sass = require('gulp-sass');
+styleguide = require('gulp-styleguide');
+
+gulp.task('styles', ['clean'], function () {
+    return gulp.src(['./layouts/styles/main.scss', './components/styles/*.scss'])
+        .pipe(sass())
+        .pipe(concat('main.css'))
+        .pipe(gulp.dest('./build/styles'));
+});
+
+gulp.task('scripts', ['clean'], function () {
+    return gulp.src(['./layouts/scripts/*.js'])
+        .pipe(concat('main.js'))
+        .pipe(gulp.dest('./build/scripts'));
+});
+
+gulp.task('clean', function() {
+    return gulp.src('./build', {read: false})
+        .pipe(clean());
+});
+
+gulp.task('styleguide', ['clean'], function () {
+    return gulp.src('./layouts/templates/*.html')
+        .pipe(styleguide())
+        .pipe(gulp.dest('./build'));
+});
+
+gulp.task('default', ['styles', 'scripts', 'styleguide']);
